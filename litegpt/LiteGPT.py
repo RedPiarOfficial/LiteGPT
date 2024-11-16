d6f315e68d8acdf6871b100cb123287965cb898a
Red <155652932+RedPiarOfficial@users.noreply.github.com>
2024-11-16 10:33:57 +0200

    Add files via upload

--- litegpt/LiteGPT.py
+++ litegpt/LiteGPT.py
@@ -1,35 +1,38 @@
 import httpx
-import random
 import re
 import json
 import logging
 
 from .lib import History
-
+from .lib.exceptions import checkErrorStatus
 logging.basicConfig(level=logging.WARNING, format='%(asctime)s - %(levelname)s - %(message)s')
 class LiteGPT:
 	def __init__(self, http2=False):
 		self.requests = httpx.Client(http2=http2)
 
 	def ask(self, prompt, history: History = None):
-
 		if history is not None and type(history) is not History:
 			logging.warning("Передано недопустимое значение для 'history': ожидался тип 'History' или значение не должно быть None.")
 			return False
 		pattern = r'data:\s*{\s*"data":\s*"(.*?)"\s*}'
-		history = history.history
-		history.append({"role": "user", "content": str(prompt)})
+		
+		if type(history) is History:
+			history = history.history
+			history.append({"role": "user", "content": str(prompt)})
 
-		resp = self.requests.post("https://twitterclone-4e8t.onrender.com/api/chat",
+		resp = self.requests.post("https://twitterclone-8wd1.onrender.com/api/chat",
 									timeout=30,
-									headers={"origin": "https://www.aiuncensored.info"},
+									headers={
+												"origin": "https://www.aiuncensored.info",
+											},
 									json={
-											"cipher": str(random.randint(1111111111111111, 9999999999999999)),
+											"cipher": "0000000000000000",
 											"messages": [{"role": "user", "content": str(prompt)}] if history == None else history
 										}
 									)
 		extracted_elements = re.findall(pattern, resp.text)
 		combined_message = ''.join(json.loads(f'"{element}"') for element in extracted_elements)
+		checkErrorStatus(resp.status_code)
 		return combined_message
 
 	def image(self, prompt):
@@ -37,8 +40,9 @@ class LiteGPT:
 									timeout=30,
 									headers={"origin": "https://www.aiuncensored.info"},
 									json={
-											"cipher": str(random.randint(1111111111111111, 9999999999999999)),
+											"cipher": "0000000000000000",
 											"prompt": str(prompt)
 										}
 									)
+		checkErrorStatus(resp.status_code)
 		return resp.json()
--- litegpt/lib/History.py
+++ litegpt/lib/History.py
@@ -1,3 +1,7 @@
+import os
+import json
+from typing import Union
+
 class History:
 	def __init__(self):
 		self._history = []
@@ -10,12 +14,34 @@ class History:
 		self._history.pop(index)
 		return self._history
 
-	def insert(self, data: dict):
+	def insert(self, data: list):
 		if type(data) is list:
 			for i in data:
 				self.add(i[0], i[1])
 			return True
 		return {"Error": "Data type must is List."}
+
+	def save(self, path: Union[str, os.PathLike] = "./AutoHistory.json") -> bool:
+		dir_path = os.path.dirname(path)
+		
+		if dir_path:
+			os.makedirs(dir_path, exist_ok=True)
+		
+		try:
+			with open(path, "w", encoding="utf-8") as file:
+				json.dump(self._history, file, ensure_ascii=False, indent=4)
+			return True
+		except (OSError, json.JSONDecodeError) as e:
+			print(f"Error: {e}")
+			return False
+
+	def load(self, path: Union[str, os.PathLike] = "./AutoHistory.json") -> bool:
+		try:
+			with open(path, "r", encoding="utf-8") as file:
+				self._history = json.load(file)
+				return True
+		except FileNotFoundError:
+			return False
 
 	@property
 	def history(self):
